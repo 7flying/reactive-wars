d109ba3fcbf5d53d1e4f9afdf2f61a186f1f0d59
7flying <diez.f.irene@gmail.com>
2016-06-13 20:57:15 +0200

    Add Tile, Map and AnimationHandler

---
+++ Tile.hpp
@@ -0,0 +1,52 @@
+#ifndef TILE_HPP
+#define TILE_HPP
+
+#include <SFML/Graphics.hpp>
+#include <vector>
+#include <string>
+#include "AnimationHandler.hpp"
+
+
+using namespace std;
+
+
+enum class TileType {
+    VOID, GRASS, FOREST, WATER, RESIDENTIAL, COMMERCIAL, INDUSTRIAL, ROAD};
+
+string tileTypeToStr(TileType type);
+
+class Tile {
+public:
+    AnimationHandler animHandler;
+    Sprite sprite;
+    TileType tileType;
+    // This allows different looking versions of the same tile
+    int tileVariant;
+    /** Region IDs of the tile, tiles in the same region are connected**/
+    // TODO: first is for transport?
+    unsigned int regions[1];
+    unsigned int cost;
+
+    // TODO: --- delete ---
+    double population;
+    unsigned int maxPopPerLevel;
+    unsigned int maxLevels;
+    float production;
+    float storedGoods;
+    // --- end delete --
+    
+    // Modificator to the characters defence, avoid and movement
+    int defence, avoid, movement;
+
+    Tile();
+    Tile(const unsigned int tileSize, const unsigned int height,
+         Texture &texture, const vector<Animation> &animations,
+         const TileType tileType, const unsigned int cost,
+         const unsigned int maxPopPerLevel, const unsigned int maxLevels);
+
+    void draw(RenderWindow &window, float dt);
+    void update();
+    string getCost() { return to_string(this->cost); }
+};
+
+#endif // TILE_HPP
